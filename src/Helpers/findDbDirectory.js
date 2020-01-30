9560cf848e2a1bc56c381eed4d8072f1e94dafee
alicanerdurmaz <alicanerdurmaz@gmail.com>
2020-01-30 21:42:12 +0300

    if db not exist. crash bug fixed

--- src/Helpers/findDbDirectory.js
+++ src/Helpers/findDbDirectory.js
@@ -1,10 +1,17 @@
 const { app } = require('electron').remote;
 const path = require('path');
+const fs = require('fs');
 
 const findDbDirectory = () => {
 	const folderName = 'custom-db';
 	const dir = app.getPath('userData');
 	const result = path.join(dir, path.sep, folderName);
+
+	try {
+		if (!fs.existsSync(result)) {
+			fs.mkdirSync(result);
+		}
+	} catch (err) {}
 
 	return result;
 };
